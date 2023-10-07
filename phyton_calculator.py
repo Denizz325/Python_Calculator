4124ca07fc9b221c84e0fdc875a5dd568318e5fb
Denizz325 <09aq.abv@gmail.com>
2023-10-07 23:32:42 +0300

    updated

---
+++ phyton_calculator.py
@@ -0,0 +1,19 @@
+def add_function(a, b: float) -> float:
+    return a + b
+
+
+def subtract_function(a, b: float) -> float:
+    return a - b
+
+
+def multiplication_function(a, b: float) -> float:
+    return a * b
+
+
+def division_function(a, b: float) -> float:
+    return a / b
+
+
+
+first_number = float(input("Enter the first number:"))
+second_number = float(input("Enter the second number:"))
